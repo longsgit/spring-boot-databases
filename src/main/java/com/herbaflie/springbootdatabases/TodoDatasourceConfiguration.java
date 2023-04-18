25a16e098e340ece69f0923766bf82b22b01f244
longs-a <longs@herbalife.com>
2023-04-17 20:21:24 -0700

    spring boot with multiple databases

---
+++ src/main/java/com/herbaflie/springbootdatabases/TodoDatasourceConfiguration.java
@@ -0,0 +1,32 @@
+package com.herbaflie.springbootdatabases;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TodoDatasourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.todos")
+    public DataSourceProperties todosDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource todosDataSource() {
+        return todosDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate todosJdbcTemplate(@Qualifier("todosDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
---
+++ src/test/java/com/herbaflie/springbootdatabases/SpringBootDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbaflie.springbootdatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatabasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
